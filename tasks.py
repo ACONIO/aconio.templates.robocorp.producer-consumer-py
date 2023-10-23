0ad8e1a83ba0de6f94ff6a0b7a2a6a3156cbb615
Matthias Reumann <matthias.reumann@tum.de>
2023-10-23 07:20:10 +0200

    Use 'with' in producer and consumer

--- tasks.py
+++ tasks.py
@@ -5,7 +5,8 @@ from robocorp.tasks import task
 from robocorp import workitems
 from robocorp import log
 
-from bot.common import setup, teardown
+from bot.context import RunContext, RobotType
+from bot.common import cleanup_robot_tmp_folder
 from bot.consumer import run as run_consumer
 from bot.producer import run as run_producer
 
@@ -14,36 +15,28 @@ from bot.producer import run as run_producer
 def producer():
     """Create output work items for the consumer."""
 
-    try:
-        setup()
-        wi_payloads = run_producer()
+    ctx = RunContext(RobotType.PRODUCER)  # Change the constructor arguments as wished.
+
+    with ctx:
+        cleanup_robot_tmp_folder(ctx.cfg.TEMP_DIR)
+
+        wi_payloads = run_producer(ctx)
 
         for payload in wi_payloads:
             workitems.outputs.create(payload)
-    except Exception as err:
-        log.exception(str(err))
-    finally:
-        teardown()
 
 
 @task
 def consumer():
     """Process all the work items created by the producer."""
 
-    try:
-        setup()
-        for item in workitems.inputs:
-            try:
-                run_consumer()
-                item.done()
+    ctx = RunContext(RobotType.CONSUMER)  # Change the constructor arguments as wished.
 
-            # TODO: adapt exceptions below
-            except AssertionError as err:
-                item.fail("BUSINESS", code="INVALID_ORDER", message=str(err))
-            except KeyError as err:
-                item.fail("APPLICATION", code="MISSING_FIELD",
-                          message=str(err))
-    except Exception as err:
-        log.exception(str(err))
-    finally:
-        teardown()
+    with ctx:
+        for item in workitems.inputs:
+            with item:
+                cleanup_robot_tmp_folder(ctx.cfg.TEMP_DIR)
+
+                run_consumer(ctx, item)
+
+                item.done()
