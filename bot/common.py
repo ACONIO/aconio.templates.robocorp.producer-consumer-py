e6dd50d7d03fca564a07edbe478fe8ca1edc6ea3
Patrick Krukenfellner <patrick.krukenfellner@aconio.net>
2023-11-05 08:38:39 +0100

    fix cleanup temp robot path func

--- bot/common.py
+++ bot/common.py
@@ -1,11 +1,21 @@
 """This file contains shared functions used by multiple tasks."""
 
 import os
+import shutil
+
+from robocorp import log
 
 
 def cleanup_robot_tmp_folder(temp_dir: str):
     """Removes all documents from the robot temp folder to cleanup previous runs."""
     for file_name in os.listdir(temp_dir):
-        file = temp_dir + file_name
-        if os.path.isfile(file):
-            os.remove(file)
+        file = os.path.join(temp_dir, file_name)
+        try:
+            if os.path.isfile(file) or os.path.islink(file):
+                log.debug(f'removing file {file}')
+                os.remove(file)
+            elif os.path.isdir(file):
+                log.debug(f'removing directory {file}')
+                shutil.rmtree(file)
+        except Exception as e:
+            log.warn(f'failed to delete {file}. reason: {e}')
