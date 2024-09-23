8629461a872ab970b038159590d9e8d70a633f5d
Patrick Krukenfellner <patrick.krukenfellner@aconio.net>
2024-09-23 12:29:44 +0200

    Fix 'load_cr_token.py' docstring

--- scripts/load_cr_token.py
+++ scripts/load_cr_token.py
@@ -1,19 +1,22 @@
-# pylint: skip-file
-# Performs an `rcc cloud authorize` command, grabbing the CR workspace ID from the
-# given JSON file and then setting the generated token to this JSON file.
-#
-# Usage (from project root):
-# python scripts/load_cr_token.py devdata/env-producer.json
-#
-# This will take the `RC_WORKSPACE_ID` from `devdata/env-producer.json` and perform an
-# `rcc cloud authorize` with this workspace ID. Then, it saves the generated token to
-# the `RC_API_TOKEN_V1` key in `devdata/env-producer.json`.
+"""
+Perform an `rcc cloud authorize` command, grabbing the CR workspace ID from
+the given JSON file and then setting the generated token to this JSON file.
+
+Usage (from project root):
+```bash
+python scripts/load_cr_token.py devdata/env-producer.json
+```
+
+This will take the `RC_WORKSPACE_ID` from `devdata/env-producer.json` and
+execute `rcc cloud authorize` with this workspace ID. Then, it saves the
+generated token to the `RC_API_TOKEN_V1` key in `devdata/env-producer.json`.
+"""
 
 import sys
 import json
 import subprocess
 
-with open(sys.argv[1]) as f:
+with open(sys.argv[1], encoding="utf-8") as f:
     data = json.load(f)
 
 rc_workspace_id = data["RC_WORKSPACE_ID"]
@@ -26,5 +29,5 @@ cr_token = json.loads(output)["token"]
 print(f"Generated token (valid for 120 minutes): {cr_token}")
 
 data["RC_API_TOKEN_V1"] = cr_token
-with open(sys.argv[1], "w") as f:
+with open(sys.argv[1], "w", encoding="utf-8") as f:
     json.dump(data, f)
